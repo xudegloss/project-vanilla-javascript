32581294bc15378f61e4b500b7a144acca7c172c
xudegloss <ajouunihospitalrn@naver.com>
2023-05-05 14:13:16 +0900

    반복되는 코드 반복문 이용하여 고쳐주기

--- 04. responsive navigation bar/app.js
+++ 04. responsive navigation bar/app.js
@@ -2,12 +2,16 @@ const navBtn = document.getElementById("nav__btn");
 const navContainer = document.getElementById("nav__container");
 const nav = document.getElementById("nav");
 const navTitle = document.getElementById("nav__title");
-
-const toggleFunc = () => {};
+const targetArray = [navBtn, navContainer, nav, navTitle];
 
 navBtn.addEventListener("click", () => {
-  navContainer.classList.toggle("active");
-  navBtn.classList.toggle("active");
-  nav.classList.toggle("active");
-  navTitle.classList.toggle("active");
+  // navContainer.classList.toggle("active");
+  // navBtn.classList.toggle("active");
+  // nav.classList.toggle("active");
+  // navTitle.classList.toggle("active");
+
+  // 계속 반복되는 코드가 있어서, for of 반복문을 이용하여 객체를 편리하게 반복하고, 반복되는 코드의 양을 줄여주었다.
+  for (let i of targetArray) {
+    i.classList.toggle("active");
+  }
 });
